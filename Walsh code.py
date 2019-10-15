1d3960805af504d62f6a9c49fd793a6dcd1da94f
Abhishek Salian26 <43337429+AbhishekSalian@users.noreply.github.com>
2019-10-15 23:05:11 +0530

    Add files via upload

---
+++ Walsh code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+# If we want to generate walsh code 2 so  power of 2 is 1 
+# write order of 2 for e.g 2^3 = 8 for walsh 8\
+# walsh code 8 => order 3
+# walsh code 16 => order 4
+def walsh_code(order):
+    #basic element(seed) of walsh code generator
+    W = np.array([0])
+    for i in range(order):
+        W = np.tile(W, (2, 2))
+        half = 1 << i
+        W[half:, half:] = np.logical_not(W[half:, half:])
+    return W
+
+print("Walsh code is:",walsh_code(5))
+
+
+# In[ ]:
+
+
+
+
